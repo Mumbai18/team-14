5b873bac38e5faba097bd918c7b7c629855c8528
Unknown <2015vishal.israni@ves.ac.in>
2018-07-21 15:47:02 +0530

    Student Registration

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -172,14 +172,14 @@
       </div> 
 
           <img src="images/jsg.jpg" style="float:left;width:100px;height:50px;">
-          <a class="navbar-brand" href="index.html" style="padding-left:50px;color:white;">Educon : By Jain Social Group</a> 
+          <a class="navbar-brand" href="index.blade.php" style="padding-left:50px;color:white;">Educon : By Jain Social Group</a> 
           </div> 
            
             <ul class="nav navbar-nav navbar-right"> 
               
               <!-- <li><img src="../../public/images/jsg.jpg"></li> -->
 
-              <li class="active"><a href="indsbbsbex.html">Home</a></li> 
+              <li class="active"><a href="index.blade.php">Home</a></li> 
               <li><a href="#">Services</a></li> 
               <li><a href="#">About Us</a></li>   
               <li><a href="#" data-toggle="modal" data-target="#myModal2"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li> 
@@ -189,6 +189,124 @@
         </div>   
       </nav> 
       <!-- Nav Bar Ends--> 
+
+      <!-- Image Slider Starts--> 
+      <script type="text/javascript"> 
+        $(document).ready(function() { 
+          $('#myCarousel').carousel({ 
+            interval: 5000 
+          }) 
+        }); 
+      </script> 
+           
+       
+         
+      <!-- Full Page Image Background Carousel Header --> 
+      <header id="myCarousel" class="carousel slide"> 
+          <!-- Indicators --> 
+          <ol class="carousel-indicators"> 
+              <li data-target="#myCarousel" data-slide-to="0" class="active"></li> 
+              <li data-target="#myCarousel" data-slide-to="1"></li> 
+              <li data-target="#myCarousel" data-slide-to="2"></li> 
+              <li data-target="#myCarousel" data-slide-to="3"></li> 
+              <li data-target="#myCarousel" data-slide-to="4"></li> 
+              <li data-target="#myCarousel" data-slide-to="5"></li> 
+              <li data-target="#myCarousel" data-slide-to="6"></li> 
+              <li data-target="#myCarousel" data-slide-to="7"></li> 
+              <li data-target="#myCarousel" data-slide-to="8"></li> 
+              <li data-target="#myCarousel" data-slide-to="11"></li> 
+              <li data-target="#myCarousel" data-slide-to="10"></li> 
+               
+          </ol> 
+       
+          <!-- Wrapper for Slides --> 
+          <div class="carousel-inner"> 
+              <div class="item active"> 
+                  <!-- Set the first background image using inline CSS below. --> 
+                  <img src="images/image2.jpg" style="width:100%; height:450px;"> 
+                                 </div> 
+              <div class="item"> 
+                  <!-- Set the second background image using inline CSS below. --> 
+                  <img src="image2.jpg" style="width:100%; height:450px;"> 
+              </div> 
+              <div class="item"> 
+                  <!-- Set the third background image using inline CSS below. --> 
+                  <img src="image3.jpg" style="width:100%; height:450px;"> 
+              </div> 
+       
+              <div class="item"> 
+                  <!-- Set the third background image using inline CSS below. --> 
+                  <img src="university4.jpeg" style="width:100%; height:450px;"> 
+              </div> 
+       
+              <div class="item"> 
+                  <!-- Set the third background image using inline CSS below. --> 
+                  <img src="university5.jpeg" style="width:100%; height:450px;"> 
+              </div> 
+       
+              <div class="item"> 
+                      <!-- Set the third background image using inline CSS below. --> 
+                      <img src="university6.jpeg" style="width:100%; height:450px;"> 
+              </div> 
+       
+              <div class="item"> 
+                      <!-- Set the third background image using inline CSS below. --> 
+                      <img src="university7.jpeg" style="width:100%; height:450px;"> 
+              </div> 
+       
+              <div class="item"> 
+                      <!-- Set the third background image using inline CSS below. --> 
+                      <img src="university8.jpeg" style="width:100%; height:450px;"> 
+              </div> 
+       
+              <div class="item"> 
+                      <!-- Set the third background image using inline CSS below. --> 
+                      <img src="university9.jpeg" style="width:100%; height:450px;"> 
+              </div> 
+       
+              <div class="item"> 
+                      <!-- Set the third background image using inline CSS below. --> 
+                      <img src="university10.jpeg" style="width:100%; height:450px;"> 
+              </div> 
+       
+              <div class="item"> 
+                      <!-- Set the third background image using inline CSS below. --> 
+                      <img src="university11.jpeg" style="width:100%; height:450px;"> 
+              </div> 
+       
+       
+          </div> 
+ 
+      <!-- Controls --> 
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev"> 
+            <span class="icon-prev"></span> 
+        </a> 
+        <a class="right carousel-control" href="#myCarousel" data-slide="next"> 
+            <span class="icon-next"></span> 
+        </a> 
+     
+    </header><style> 
+        button{ 
+            padding:0px; 
+            margin:0px; 
+            background:none; 
+            border:none; 
+        } 
+        button:focus {outline:0;} 
+    </style> 
+    <script> 
+    $(document).ready(function(){ 
+        $("#post").click(function(){ 
+            $("#box").load("post.php"); 
+        }); 
+      $("#profile").click(function(){ 
+            $("#box").load("profile.php"); 
+        }); 
+    }); 
+    </script> 
+     
+<!-- Image Slider Ends--> 
+
        
        
       
