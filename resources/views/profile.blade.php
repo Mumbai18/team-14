43023351fb4cb62bd9a1c174118e2793975a2ac2
MitP97 <mitvparekh9@yahoo.com>
2018-07-21 22:21:30 +0530

    minor changes to templating

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,6 +1,5 @@
-<?php
-	include('nav_side.blade.php');
-
+@include(nav_side)
+@section
 	<div class="container" style="width:100%">
         <div class="row">
         <br>
@@ -13,19 +12,19 @@
         <br>
         </div>
             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
-    
-    
+
+
             <div class="panel panel-primary">
                 <div class="panel-heading">
                 <h3 class="panel-title"><?php echo $fname, " ", $lname ?></h3>
                 </div>
                 <div class="panel-body">
                 <div class="row">
-                    <div class="col-md-3 col-lg-3 " align="center" > 
+                    <div class="col-md-3 col-lg-3 " align="center" >
                     <?php echo '<img style="width:150px; height:175px;" class="img-rounded img-responsive" src="data:image/jpeg;base64,'.base64_encode( $row1['User_Picture'] ).'"/>';?>
                     <!--<img alt="User Pic" src=""" class="img-rounded img-responsive"> </div>-->
                     </div>
-                    
+
                     <!--<div class="col-xs-10 col-sm-10 hidden-md hidden-lg"> <br>
                     <dl>
                         <dt>DEPARTMENT:</dt>
@@ -38,15 +37,15 @@
                         <dd>Male</dd>
                     </dl>
                     </div>-->
-                    <div class=" col-md-9 col-lg-9 "> 
+                    <div class=" col-md-9 col-lg-9 ">
                     <table class="table table-user-information">
                         <tbody>
-                        
+
                         <tr>
                             <td>Date of Birth</td>
                             <td><?php echo $dob ?></td>
                         </tr>
-                    
+
                             <tr>
                                 <tr>
                             <td>Gender</td>
@@ -62,12 +61,12 @@
                         </tr>
                             <td>Phone Number</td>
                             <td><?php echo $phone_no ?></td>
-                            
+
                         </tr>
-                        
+
                         </tbody>
                     </table>
-                    
+
                     <a href="application_status.php" class="btn btn-success">My Application Status</a>
                     <a href="edit_profile.php" class="btn btn-primary">Edit Profile</a>
                     <a href="logout.php" class="btn btn-danger">Log Out</a>
@@ -75,15 +74,14 @@
                 </div>
                 </div>
                     <div class="panel-footer">
-                        
+
                             <a data-original-title="Broadcast Message" data-toggle="tooltip" type="button" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-envelope"></i></a>
                             <span class="pull-right">
                                 <a href="edit_profile.html" data-original-title="Edit this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
                                 <a data-original-title="Remove this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-remove"></i></a>
                             </span>
                         </div>
-                
+
             </div>
             </div>
-    
-?>
+    @endsection
