d0859717a348bdfd602122f0d727e47ef843ca88
MitP97 <mitvparekh9@yahoo.com>
2018-07-22 02:58:04 +0530

    fix

--- app/Http/Controllers/GlobalsController.php
+++ app/Http/Controllers/GlobalsController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-use User;
-use Document;
+use App\User;
+use App\Document;
 use DB;
 use Twilio\Rest\Client;
 
