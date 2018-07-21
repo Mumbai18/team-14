4787d85218a7ea81850cb948ea1ccb57af8f0d42
Unknown <2015vishal.israni@ves.ac.in>
2018-07-21 17:56:39 +0530

    Final documents upload

--- resources/views/academic_documents.blade.php
+++ resources/views/academic_documents.blade.php
@@ -9,31 +9,6 @@
 <body>
 
 <h2>Upload Personal details</h2>
-    <div class="container" style="width:100%;">  
-        <h3>Aadhar Card</h3>  
-        <br/>
-        <div class="col-md-12">  
-            <form method="post" action="Passport_Backend.php" enctype="multipart/form-data">  
-                <input type="file" class="col-md-4" name="Passport" id="image"/>
-                <div class="col-md-6">
-                   <hr> 
-                </div> 
-            </form>
-            <hr>
-     </div>
-        
-     <div class="container" style="width:100%;">  
-        <h3>Pan Card</h3>  
-        <br/>
-        <div class="col-md-12">  
-            <form method="post" action="Passport_Backend.php" enctype="multipart/form-data">  
-                <input type="file" class="col-md-4" name="Passport" id="image"/>
-                <div class="col-md-6">
-                   <hr> 
-                </div> 
-            </form>
-            <hr>
-     </div>
     
     
     <div class="container" style="width:100%;">  
@@ -50,7 +25,7 @@
      </div>
     
     <div class="container" style="width:100%;">  
-        <h3>Passport</h3>  
+        <h3>Income Certificate</h3>  
         <br/>
         <div class="col-md-12">  
             <form method="post" action="Passport_Backend.php" enctype="multipart/form-data">  
@@ -63,7 +38,7 @@
      </div>
     
     <div class="container" style="width:100%;">  
-        <h3>Transcript</h3>  
+        <h3>Salary Slip</h3>  
         <br/>
         <div class="col-md-12">  
             <form method="post" action="Passport_Backend.php" enctype="multipart/form-data">  
